8c32d4bec3e7c48433581d5437ecccce2aef5312
Sudoharry <shambudanb@gmail.com>
2024-11-28 05:39:10 +0000

    Feat: Added basic program which can check whether the number is even or odd

---
+++ Basic/check-two-no-even-or-odd./check-two-no-even-or-odd.py
@@ -0,0 +1,9 @@
+# Program  to check the number is even or odd
+
+number = int(input("Enter the number:"))
+
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
