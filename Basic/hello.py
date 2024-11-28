6a1a3b0541b9d0e4ce50c564f28ece7388b21080
Sudoharry <shambudanb@gmail.com>
2024-11-28 04:56:44 +0000

    Feat: My first Python program

---
+++ Basic/hello.py
@@ -0,0 +1,11 @@
+#This is the basic Python Program
+#
+#
+#It asks you name and greets you
+#
+
+# Taking inputs from the user
+name = input ("What is your name?")
+
+# Display a greeting message
+print (f"Hello, {name}! Welcome to NextGen Python Labs.")
